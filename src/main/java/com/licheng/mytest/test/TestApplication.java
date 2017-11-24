7f003e4c6620b7ce5f1d0af773a45757741ebfc0
garen <garen_li_0725@163.com>
2017-11-25 00:01:07 +0800

    update code

--- src/main/java/com/licheng/mytest/test/TestApplication.java
+++ src/main/java/com/licheng/mytest/test/TestApplication.java
@@ -2,11 +2,19 @@ package com.licheng.mytest.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String sayHello(){
+		return "hello springboot";
 	}
 }
